3dc154417519db62fbb417ee8724b85e83979a7c
unknown <andrew.ning.cs@gmail.com>
2020-04-19 15:53:20 -0400

    Ok

--- server.py
+++ server.py
@@ -28,7 +28,7 @@ class Battlesnake(object):
         # TODO: Use this function to decide how your snake is going to look on the board.
         data = cherrypy.request.json
         print("START")
-        return {"color": "#888888", "headType": "regular", "tailType": "regular"}
+        return {"color": "#888888", "headType": "bendr", "tailType": "regular"}
 
     @cherrypy.expose
     @cherrypy.tools.json_in()
