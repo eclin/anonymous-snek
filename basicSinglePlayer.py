642969ebdbff6bd39610fc6d9455de2eb7d06103
unknown <andrew.ning.cs@gmail.com>
2020-04-20 12:49:46 -0400

    Removed json.loads

--- basicSinglePlayer.py
+++ basicSinglePlayer.py
@@ -1,13 +1,10 @@
-import json
-
 def move(data):
-    parsedData = json.loads(data)
-    if parsedData["you"]["body"][0]["x"] < parsedData["board"]["food"][0]["x"]:
+    if data["you"]["body"][0]["x"] < data["board"]["food"][0]["x"]:
         return "right"
-    elif parsedData["you"]["body"][0]["x"] > parsedData["board"]["food"][0]["x"]:
+    elif data["you"]["body"][0]["x"] > data["board"]["food"][0]["x"]:
         return "left"
     else:
-        if parsedData["you"]["body"][0]["y"] < parsedData["board"]["food"][0]["y"]:
+        if data["you"]["body"][0]["y"] < data["board"]["food"][0]["y"]:
             return "up"
         else:
             return "down"
