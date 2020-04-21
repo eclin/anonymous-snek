c2133a0eef006b9fcd3e83416f36f4328691259c
Eric Lin <c84lin@uwaterloo.ca>
2020-04-21 17:32:03 -0400

    hopefully works for many snakes at the same time

--- BasicSnake.py
+++ BasicSnake.py
@@ -6,12 +6,16 @@ from basicSinglePlayer import BasicStrategy
 from tests.BattlefieldTest import BattfieldTest
 
 class BasicSnake(Snake):
+    def __init__(self):
+        self.strategy = {}
+
     @cherrypy.expose
     @cherrypy.tools.json_in()
     @cherrypy.tools.json_out()
     def start(self):
         data = cherrypy.request.json
-        self.strategy = BasicStrategy(data)
+        snake_id = data['you']['id']
+        self.strategy[snake_id] = BasicStrategy(data)
         print("START")
         print(data)
         return {"color": "#888889", "headType": "bendr", "tailType": "sharp"}
@@ -21,9 +25,9 @@ class BasicSnake(Snake):
     @cherrypy.tools.json_out()
     def move(self):
         data = cherrypy.request.json
-
-        self.strategy.update(data)
-        move = self.strategy.basic_move()
+        snake_id = data['you']['id']
+        self.strategy[snake_id].update(data)
+        move = self.strategy[snake_id].basic_move()
 
         print(data)
         print(f"MOVE: {move}")
