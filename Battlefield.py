794d10b1ac6a6d87ee8a4a4b76599dc15871b637
Eric Lin <c84lin@uwaterloo.ca>
2020-04-21 14:43:49 -0400

    pog

--- Battlefield.py
+++ Battlefield.py
@@ -1,3 +1,4 @@
+import math
 
 LEFT = 'left'
 RIGHT = 'right'
@@ -22,6 +23,15 @@ class Coord(object):
     def right(self):
         return Coord(self.x+1, self.y)
 
+    def distance(self, p):
+        return abs(self.x - p.x) + abs(self.y - p.y)
+
+    def __eq__(self, p):
+        return self.x == p.x and self.y == p.y
+
+    def __ne__(self, p):
+        return not self.__eq__(p)
+
 class Board(object):
     # data is the entire game json dictionary.
     def __init__(self, data):
@@ -43,14 +53,31 @@ class Board(object):
         self.food = []
         for f in data['board']['food']:
             self.food.append(Coord(f['x'], f['y']))
+        self.my_snake.update(data['you'])
+        self.other_snakes = []
+        for s in data['board']['snakes']:
+            curID = s['id']
+            if curID != self.my_snake.id:
+                for o in self.other_snakes:
+                    if curID == o.id:
+                        o.update(s)
 
     def out_of_bounds(self, p):
         if p.x < 0 or p.x >= self.width or p.y < 0 or p.y >= self.height:
             return True
         return False
 
+    # returns coord of closest food to your snake head
+    def closest_food(self):
+        low = self.height + self.width
+        p = self.food[0]
+        for f in self.food:
+            if self.my_snake.head.distance(f) < low:
+                p = f
+        return
+
     # return possible moves given a position (default to your head)
-    # moves returned could still lead to death
+    # moves returned could still lead to death (like head on head)
     def possible_moves(self, point=None):
         if point is None:
             point = self.my_snake.head
@@ -64,14 +91,18 @@ class Board(object):
             if self.out_of_bounds(moves[d]):
                 moves.pop(d)
 
-        # Avoid crashing into other snake bodies/your own body
+        # Avoid crashing into other snake bodies/your own body,
+        # except for the tail if it were to move
         for s in self.other_snakes:
             for d in DIRECTIONS:
                 if moves[d] in s.body:
+                    # if its to a tail spot and the tail will move, then its ok
+                    if moves[d] == s.tail and not s.will_extend:
+                        continue
                     moves.pop(d)
         
         for d in DIRECTIONS:
-            if self.my_snake.length > 1 and moves[d] == self.my_snake.body[1]:
+            if moves[d] in self.my_snake.body:
                 moves.pop(d)
         
         return [moves[d] for d in DIRECTIONS]
@@ -83,10 +114,11 @@ class Board(object):
         new_moves = []
         # check if can collide head on with another equal sized or longer snake
         for s in self.other_snakes:
-            other_moves = s.possible_moves()
-            for m in moves:
-                if m not in other_moves:
-                    new_moves.append(m)
+            if s.length >= self.my_snake.length:
+                other_moves = s.possible_moves()
+                for m in moves:
+                    if m not in other_moves:
+                        new_moves.append(m)
 
         return new_moves
 
@@ -107,6 +139,27 @@ class Snake(object):
         self.length = len(self.body)
         self.will_extend = True
         self.just_ate = True
+
+    # updates a snake
+    def update(self, data):
+        self.health = data['health']
+
+        first = Coord(data['body'][0]['x'], data['body'][0]['y'])
+        last = Coord(data['body'][-1]['x'], data['body'][-1]['y'])
+        self.body.insert(0, first)
+        if len(data['body']) > self.length:
+            # just ate a food -> pop back, add new back
+            self.body.pop(-1)
+            self.body.append(last)
+            self.just_ate = True
+            self.will_extend = True
+
+        self.head = self.body[0]
+        self.tail = self.body[-1]
+        self.length = len(self.body)
+
+        if (self.length < 3):
+            self.will_extend = True
 
     # returns possible moves of the snake. Does not take into account bad moves.
     def possible_moves(self):
