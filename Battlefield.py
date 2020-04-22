c5a97d3ecc0047da534c1673d3cba2d3a2d2b534
unknown <andrew.ning.cs@gmail.com>
2020-04-21 20:07:03 -0400

    Added logging

--- Battlefield.py
+++ Battlefield.py
@@ -171,6 +171,7 @@ class Snake(object):
         last = Coord(data['body'][-1]['x'], data['body'][-1]['y'])
         self.body.insert(0, first)
         if len(data['body']) >= self.size_on_board():
+            print("Ate, new tail: ({last.x},{last.y})")
             # just ate a food -> pop back, add new back
             self.body.pop(-1)
             self.body.append(last)
