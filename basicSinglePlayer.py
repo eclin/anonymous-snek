df3acecbb1da9becfb22b1b443722f5c32366548
unknown <andrew.ning.cs@gmail.com>
2020-04-22 18:22:33 -0400

    Please work

--- basicSinglePlayer.py
+++ basicSinglePlayer.py
@@ -8,11 +8,17 @@ class BasicStrategy(object):
     def update(self, data):
         self.board.update(data)
 
-    def findBestMove(self, beneficial):
+    def findBestMove(self, beneficial, target):
         for areas in self.board.areas:
             for move in beneficial:
-                if move in areas:
-                    return move, len(areas)
+                if move in areas and len(areas) >= target:
+                    return move
+        for areas in self.board.areas:
+            for move in beneficial:
+                turns, newsize = self.board.turns_to_open(move)
+                if newsize >= target and len(areas) > turns:
+                    return move
+        return None
 
     def basic_move(self):
         moves = self.board.possible_moves()
@@ -45,15 +51,14 @@ class BasicStrategy(object):
             # if health is greater than threshold then dont take a riskier move
             if beneficial:
                 print(f"Beneficial Safe Moves: ({[self.board.my_snake.head.direction(x) for x in beneficial]})")
-                move_to_take, area = self.findBestMove(beneficial)
-                print(f"Area of potential new beneficial safe area: {area}")
-                if area > self.board.my_snake.length or self.board.my_snake.health < 2 * (self.board.height + self.board.width):
+                move_to_take = self.findBestMove(beneficial, self.board.my_snake.length)
+                if move_to_take != None:
                     return self.board.my_snake.head.direction(move_to_take)
-            if self.board.my_snake.health >= 2 * (self.board.height + self.board.width):
+            if self.board.my_snake.health >= (self.board.height + self.board.width):
                 print(f"Nothing beneficial and not starving")
-                move_to_take, area = self.findBestMove(moves_no_death)
-                print(f"Area of potential new safe area: {area}")
-                return self.board.my_snake.head.direction(move_to_take)
+                move_to_take = self.findBestMove(moves_no_death, self.board.my_snake.length)
+                if move_to_take != None:
+                    return self.board.my_snake.head.direction(move_to_take)
         # moves is not empty
         print("Making a risky move")
         beneficial.clear()
@@ -68,10 +73,10 @@ class BasicStrategy(object):
         # TODO: can potentially mark certain moves as less dangerous and take the least dangerous move
         if beneficial:
             print(f"Beneficial Risky Moves: ({[self.board.my_snake.head.direction(x) for x in beneficial]})")
-            move_to_take, area = self.findBestMove(beneficial)
-            print(f"Area of potential new risky area: {area}")
+            move_to_take = self.findBestMove(beneficial, self.board.my_snake.length)
+            if move_to_take != None:
+                return self.board.my_snake.head.direction(move_to_take)
+        move_to_take = self.findBestMove(moves, self.board.my_snake.length)
+        if move_to_take != None:
             return self.board.my_snake.head.direction(move_to_take)
-        else:
-            move_to_take, area = self.findBestMove(moves)
-            print(f"Area of potential new risky death area: {area}")
-            return self.board.my_snake.head.direction(move_to_take)
+        return self.board.my_snake.head.direction(random.choice(moves))
